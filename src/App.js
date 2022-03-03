221c80765fe56c805bd89687890ef4909af90f83
Syndicationa <janepmazurek@gmail.com>
2022-03-03 12:19:00 -0500

    Adds the header

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header>
+            <div class="logo"><img src="image2vector.svg" /></div> 
+            <span>Joseph Mazurek - Syndicatus</span>
+           <nav><button id="Home">Home</button><button id="ContInfo">Contact</button></nav>
+        </header>
     </div>
   );
 }
